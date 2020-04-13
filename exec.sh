9a04f6463e1dbdb1c0d5619e09f21bf20b620023
ginevracoal <ginevracoal@gmail.com>
2020-04-13 17:51:39 +0200

    samples input + new script

--- exec.sh
+++ exec.sh
@@ -2,16 +2,21 @@
 
 # ------ settings -------- #
 
-INPUTS="60000"
+INPUTS="10000"
 DATASET="mnist" # mnist, cifar, fashion_mnist
 DEVICE="cuda" # cpu, cuda
-INFERENCE="svi" # svi, hmc
 
-EPOCHS=300
-LR=0.0001
+ARCHITECTURE="conv" # fc, fc2, conv
+ACTIVATION="leaky" # leaky, sigm, tanh
+HIDDEN_SIZE=32
 
-HMC_SAMPLES=10
-WARMUP=100
+INFERENCE="hmc" # svi, hmc
+
+EPOCHS=50
+LR=0.001
+
+WARMUP=10
+N_SAMPLES=20
 
 ATTACK="fgsm" # fgsm, pgd
 
@@ -26,9 +31,12 @@ TESTS="tests/$DATE/"
 mkdir -p $TESTS
 OUT="${TESTS}${TIME}_out.txt"
 
-# python3 reducedBNN.py --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --hmc_samples=$HMC_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
-# python3 lossGradients.py  --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --hmc_samples=$HMC_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
-# python3 plot.py  --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --hmc_samples=$HMC_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
-python3 adversarialAttacks.py --attack=$ATTACK --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --hmc_samples=$HMC_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
+python3 bnn.py --inputs=$INPUTS --dataset=$DATASET --architecture=$ARCHITECTURE --activation=$ACTIVATION --hidden_size=$HIDDEN_SIZE --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --samples=$N_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
+
+# python3 reducedBNN.py --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --samples=$N_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
+# python3 adversarialAttacks.py --attack=$ATTACK --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --samples=$N_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
+
+# python3 lossGradients.py  --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --samples=$N_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
+# python3 plot.py  --inputs=$INPUTS --dataset=$DATASET --inference=$INFERENCE --epochs=$EPOCHS --lr=$LR --samples=$N_SAMPLES --warmup=$WARMUP --device=$DEVICE &>> $OUT
 
 deactivate
