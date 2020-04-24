e8d649b99c66be58d02a5d7db70585b0104bae9e
ginevracoal <ginevracoal@gmail.com>
2020-04-24 12:23:26 +0200

    better plot

--- plot/classwise_gradients_components.py
+++ plot/classwise_gradients_components.py
@@ -12,19 +12,18 @@ from utils import classwise_data_loaders
 
 
 def plot_gradients_components_classes(loss_gradients, n_inputs, n_samples_list, name):
-    matplotlib.rc('font', **{'weight': 'bold', 'size': 12})
-    fig, ax = plt.subplots(2, 5, figsize=(10, 6), dpi=150, facecolor='w', edgecolor='k')
+    matplotlib.rc('font', **{'weight':'bold', 'size': 8})
+    fig, ax = plt.subplots(2, 5, figsize=(11, 5), dpi=150, facecolor='w', edgecolor='k')
     sns.set_palette("gist_heat", 5)
-
+    
     for label in range(10):
-        print("\nlabel=", label)
+        print("\nlabel =", label, end="\n")
         plot_samples = []
         loss_gradients_components = []
         for samples_idx, n_samples in enumerate(n_samples_list):
             samples_grad_components = np.array(loss_gradients[samples_idx, label]).flatten()
             loss_gradients_components.extend(samples_grad_components)
             plot_samples.extend(np.repeat(n_samples, len(samples_grad_components)))
-            
 
         df = pd.DataFrame(data={"loss_gradients": loss_gradients_components, 
                                 "n_samples": plot_samples})
@@ -35,14 +34,18 @@ def plot_gradients_components_classes(loss_gradients, n_inputs, n_samples_list, 
         sns.stripplot(x="n_samples", y="loss_gradients", data=df, linewidth=-0.1, 
                       ax=axis, jitter=0.2, alpha=0.4)
 
+        axis.set_title("label = "+str(label), fontdict={"fontsize":9,"fontweight":"bold"},
+            pad=0.5)
         axis.set_ylabel("")
         axis.set_xlabel("")
         # axis.set_yscale('log')
 
-    fig.text(0.5, 0.01, "Samples involved in the expectations ($w \sim p(w|D)$)", ha='center')
-    fig.text(0.03, 0.5, r"Expected Gradients components $\langle\nabla L(x,w)\rangle_{w}$", 
-             va='center', rotation='vertical')
+    fig.text(0.5, 0., "Samples involved in the expectations ($w \sim p(w|D)$)", 
+             ha='center', size=10)
+    fig.text(0., 0.5, r"Expected Gradients components $\langle\nabla L(x,w)\rangle_{w}$", 
+             va='center', rotation='vertical', size=10)
 
+    plt.tight_layout()
     path = TESTS+name+"/"
     filename = "expLossGrads_inp="+str(n_inputs)+"_stripplot.png"
     os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -64,14 +67,20 @@ def main(args):
 
     n_samples_list = [1,10,50,100,500]
 
+    for class_idx, test_loader in enumerate(test_loaders):
+        print(f"\nlabel={class_idx}")
+        for n_samples in n_samples_list:
+            print(f"samples={n_samples}" , end="\t")
+            bnn.evaluate(test_loader=test_loader, device=args.device, n_samples=n_samples)
+
     # === compute gradients === #
 
-    for n_samples in n_samples_list:
-        for class_idx, test_loader in enumerate(test_loaders):
+    # for n_samples in n_samples_list:
+    #     for class_idx, test_loader in enumerate(test_loaders):
 
-            savedir, filename = (bnn.name+"/classwise_gradients/", "class="+str(class_idx))
-            loss_gradients(net=bnn, n_samples=n_samples, filename=filename, savedir=savedir,
-                           data_loader=test_loader, device=args.device)
+    #         savedir, filename = (bnn.name+"/classwise_gradients/", "class="+str(class_idx))
+    #         loss_gradients(net=bnn, n_samples=n_samples, filename=filename, savedir=savedir,
+    #                        data_loader=test_loader, device=args.device)
 
     # === load and plot === #
 
