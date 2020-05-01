5e060befca8d22afc07d1b40dcdaa190512ce236
ginevracoal <ginevracoal@gmail.com>
2020-05-01 14:19:46 +0200

    plot hidden vs samp

--- toy_example_moons.py
+++ toy_example_moons.py
@@ -23,6 +23,9 @@ from lossGradients import loss_gradients, load_loss_gradients
 import matplotlib
 
 
+DATA=DATA+"half_moons_grid_search/"
+
+
 class MoonsBNN(BNN):
     def __init__(self, hidden_size, activation, architecture, inference, 
                  epochs, lr, n_samples, warmup, n_inputs, input_shape, output_size):
@@ -40,10 +43,10 @@ def _train_and_compute_grads(hidden_size, activation, architecture, inference,
     bnn = MoonsBNN(hidden_size, activation, architecture, inference, 
                    epochs, lr, n_samples, warmup, n_inputs, inp_shape, out_size)
     bnn.train(train_loader=train_loader, device="cpu")
-    # bnn.load(device="cpu", rel_path=TESTS)
+    # bnn.load(device="cpu", rel_path=DATA)
 
     _, test_loader, _, _ = \
-        data_loaders(dataset_name="half_moons", batch_size=64, n_inputs=10000, shuffle=True)
+        data_loaders(dataset_name="half_moons", batch_size=64, n_inputs=1000, shuffle=True)
 
     loss_gradients(net=bnn, n_samples=posterior_samples, savedir=bnn.name+"/", 
                     data_loader=test_loader, device="cpu", filename=bnn.name)
@@ -60,8 +63,9 @@ def parallel_grid_search(hidden_size, activation, architecture, inference,
         delayed(_train_and_compute_grads)(*init) for init in combinations)
 
 
-def build_dataset(hidden_size, activation, architecture, inference, epochs, lr, n_samples, 
-                  warmup, n_inputs, posterior_samples, device="cuda", test_points=10000):
+def build_components_dataset(hidden_size, activation, architecture, inference, epochs, lr, 
+                            n_samples,  warmup, n_inputs, posterior_samples, device="cuda", 
+                            test_points=1000, rel_path=TESTS):
 
     _, test_loader, inp_shape, out_size = \
         data_loaders(dataset_name="half_moons", batch_size=64, n_inputs=test_points, shuffle=True)
@@ -71,55 +75,199 @@ def build_dataset(hidden_size, activation, architecture, inference, epochs, lr, 
     
     cols = ["hidden_size", "activation", "architecture", "inference", "epochs", "lr", 
             "n_samples", "warmup", "n_inputs", "posterior_samples", "test_acc",
-            "loss_gradients_components"]
+            "loss_gradients_x","loss_gradients_y"]
     df = pandas.DataFrame(columns=cols)
 
     row_count = 0
     for init in combinations:
 
         bnn = MoonsBNN(*init, inp_shape, out_size)
-        bnn.load(device=device, rel_path=TESTS)
+        bnn.load(device=device, rel_path=rel_path)
         
         for p_samp in posterior_samples:
             bnn_dict = {cols[k]:val for k, val in enumerate(init)}
 
             test_acc = bnn.evaluate(test_loader=test_loader, device=device, n_samples=p_samp)
             loss_grads = load_loss_gradients(n_samples=p_samp, filename=bnn.name, 
-                                             savedir=bnn.name+"/", relpath=TESTS) 
-            loss_gradients_components = loss_grads[:test_points].flatten()
+                                             savedir=bnn.name+"/", relpath=rel_path) 
+            loss_gradients_components = loss_grads[:test_points]
             for value in loss_gradients_components:
                 bnn_dict.update({"posterior_samples":p_samp, "test_acc":test_acc,
-                                 "loss_gradients_components":value})
+                                 "loss_gradients_x":value[0], "loss_gradients_y":value[1]})
                 df.loc[row_count] = pandas.Series(bnn_dict)
                 row_count += 1
 
     print("\nSaving:", df.head())
+
+    os.makedirs(os.path.dirname(TESTS), exist_ok=True)
     df.to_csv(TESTS+"halfMoons_lossGrads_gridSearch_"+str(test_points)+".csv", 
               index = False, header=True)
     return df
 
-def scatterplot_gridSearch_gradComponents(dataset, device="cuda", test_points=100):
+def scatterplot_gridSearch_gradComponents(dataset, test_points, device="cuda"):
 
-    sns.set()
-    matplotlib.rc('font', **{'weight': 'bold', 'size': 12})
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 5), dpi=150, facecolor='w', edgecolor='k')
-    sns.set_palette("gist_heat", 5)
+    print(dataset)
+    categorical = dataset["posterior_samples"]
+    ncols = len(np.unique(categorical))
 
-    g = sns.scatterplot(data=dataset, x="test_acc", y="loss_gradients_components", 
-                        hue="posterior_samples", alpha=0.9, #style="hidden_size", 
-                        ax=ax)
-    g.set(xlim=(60, None))
+    sns.set_style("darkgrid")
+    cmap = sns.cubehelix_palette(rot=-.9, as_cmap=True)
+    matplotlib.rc('font', **{'weight': 'bold', 'size': 10})
+    fig, ax = plt.subplots(nrows=1, ncols=ncols, figsize=(16, 5), dpi=150, 
+                           facecolor='w', edgecolor='k')
 
-    ax.set_xlabel("")
-    ax.set_ylabel("")
-    fig.text(0.5, 0.01, 'Test accuracy (%) ', ha='center', fontsize=12)
-    fig.text(0.03, 0.5, 'Expected loss gradients components', va='center',fontsize=12,
-                                                               rotation='vertical')
-    # ax.legend(loc='upper right')
+    for idx, val in enumerate(np.unique(categorical)):
+        # for y in ["loss_gradients_x","loss_gradients_y"]:
+        df = dataset[categorical==val]
+        g = sns.scatterplot(data=df, x="loss_gradients_x", y="loss_gradients_y", 
+                            size="test_acc", hue="test_acc", alpha=0.85, 
+                            #sizes=(5, 100), #style="hidden_size", 
+                            ax=ax[idx], legend=False, palette=cmap)
+        ax[idx].set_title(str(val)+" samples")
+        ax[idx].set_xlabel("")
+        ax[idx].set_ylabel("")
+    # fig.text(0.5, 0.01, '(%) ', ha='center', fontsize=12)
+    # fig.text(0.03, 0.5, 'Expected loss gradients components', va='center',fontsize=12,
+    #                                                            rotation='vertical')
+    # ax.legend(loc='upper right', title="posterior samples")
+
+    fig.text(0.5, 0.01, "Samples involved in the expectations ($w \sim p(w|D)$)", ha='center')
 
     filename = "halfMoons_lossGradsComponents_"+str(test_points)+".png"
     os.makedirs(os.path.dirname(TESTS), exist_ok=True)
     plt.savefig(TESTS + filename)
+
+
+def scatterplot_gridSearch_samp_vs_hidden(dataset, test_points, device="cuda"):
+
+    print(dataset)
+    dataset = dataset[dataset["test_acc"]>60]
+
+    # dataset["avg_grad"]=dataset[['loss_gradients_x', 'loss_gradients_y']].mean(axis=1)
+    categorical_rows = dataset["hidden_size"]
+    categorical_cols = dataset["posterior_samples"]
+    nrows = len(np.unique(categorical_rows))
+    ncols = len(np.unique(categorical_cols))
+
+    sns.set_style("darkgrid")
+    cmap = "gist_heat_r"
+    # cmap = sns.cubehelix_palette(rot=-.7, as_cmap=True)
+    matplotlib.rc('font', **{'size': 10})
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 6), dpi=150, 
+                           facecolor='w', edgecolor='k')
+
+    min_acc, max_acc = dataset["test_acc"].min(), dataset["test_acc"].max()
+
+    for r, row_val in enumerate(np.unique(categorical_rows)):
+        for c, col_val in enumerate(np.unique(categorical_cols)):
+
+            df = dataset[(categorical_rows==row_val)&(categorical_cols==col_val)]
+
+            g = sns.scatterplot(data=df, x="loss_gradients_x", y="loss_gradients_y", 
+                                size="test_acc", hue="test_acc", alpha=0.8, 
+                                vmin=min_acc, vmax=max_acc, 
+                                ax=ax[r,c], legend=False, sizes=(20, 80), palette=cmap)
+            ax[r,c].set_xlabel("")
+            ax[r,c].set_ylabel("")
+            # ax[0,c].xaxis.set_label_position("top")
+            ax[-1,c].set_xlabel(str(col_val),labelpad=3,fontdict=dict(weight='bold'))
+            # ax[r,-1].yaxis.set_label_position("right")
+            ax[r,0].set_ylabel(str(row_val),labelpad=10,fontdict=dict(weight='bold')) #rotation=270,
+
+
+    ## colorbar    
+    cbar_ax = fig.add_axes([0.93, 0.08, 0.01, 0.8])
+    cbar = fig.colorbar(matplotlib.cm.ScalarMappable(norm=None, cmap=cmap), cax=cbar_ax)
+    cbar.ax.set_ylabel('Test accuracy (%)', rotation=270, fontdict=dict(weight='bold'))
+    cbar.set_ticks([0,1])
+    cbar.set_ticklabels([60,100])
+    
+    ## titles and labels
+    fig.text(0.03, 0.5, "Hidden size", va='center',fontsize=12, rotation='vertical',
+        fontdict=dict(weight='bold'))
+    fig.text(0.5, 0.01, r"Samples involved in the expectations ($w \sim p(w|D)$)", 
+        fontsize=12, ha='center',fontdict=dict(weight='bold'))
+    fig.suptitle(r"Expected loss gradients components $\langle \nabla_{x} L(x,w)\rangle_{w}$ on Half Moons dataset",
+               fontsize=12, ha='center', fontdict=dict(weight='bold'))
+
+    filename = "halfMoons_samp_vs_hidden_"+str(test_points)+".png"
+    os.makedirs(os.path.dirname(TESTS), exist_ok=True)
+    plt.savefig(TESTS + filename)
+
+def build_variance_dataset(hidden_size, activation, architecture, inference, epochs, lr, 
+                            n_samples,  warmup, n_inputs, posterior_samples, device="cuda", 
+                            test_points=1000, rel_path=TESTS):
+
+    _, test_loader, inp_shape, out_size = \
+        data_loaders(dataset_name="half_moons", batch_size=64, n_inputs=test_points, shuffle=True)
+
+    combinations = list(itertools.product(hidden_size, activation, architecture, inference, 
+                      epochs, lr, n_samples, warmup, n_inputs))
+    
+    cols = ["hidden_size", "activation", "architecture", "inference", "epochs", "lr", 
+            "n_samples", "warmup", "n_inputs", "posterior_samples", "test_acc",
+            "loss_gradients_var_x","loss_gradients_var_y"]
+    df = pandas.DataFrame(columns=cols)
+
+    row_count = 0
+    for init in combinations:
+
+        bnn = MoonsBNN(*init, inp_shape, out_size)
+        bnn.load(device=device, rel_path=rel_path)
+        
+        for p_samp in posterior_samples:
+            bnn_dict = {cols[k]:val for k, val in enumerate(init)}
+            test_acc = bnn.evaluate(test_loader=test_loader, device=device, n_samples=p_samp)
+            loss_grads = load_loss_gradients(n_samples=p_samp, filename=bnn.name, 
+                                             savedir=bnn.name+"/", relpath=rel_path) 
+            loss_grads_var = loss_grads[:test_points].var(0)
+            bnn_dict.update({"posterior_samples":p_samp, "test_acc":test_acc,
+                             "loss_gradients_var_x":loss_grads_var[0],
+                             "loss_gradients_var_y":loss_grads_var[1]})
+            df.loc[row_count] = pandas.Series(bnn_dict)
+            row_count += 1
+
+    print("\nSaving:", df.head())
+
+    os.makedirs(os.path.dirname(TESTS), exist_ok=True)
+    df.to_csv(TESTS+"halfMoons_lossGrads_compVariance_"+str(test_points)+".csv", 
+              index = False, header=True)
+    return df
+
+def scatterplot_gridSearch_variance(dataset, test_points, device="cuda"):
+
+    print(dataset)
+    categorical = dataset["hidden_size"]
+    ncols = len(np.unique(categorical))
+
+    sns.set_style("darkgrid")
+    cmap = sns.cubehelix_palette(rot=-.9, as_cmap=True)
+    matplotlib.rc('font', **{'weight': 'bold', 'size': 10})
+    fig, ax = plt.subplots(nrows=1, ncols=ncols, figsize=(16, 5), dpi=150, 
+                           facecolor='w', edgecolor='k')
+
+    for idx, val in enumerate(np.unique(categorical)):
+        for y in ["loss_gradients_var_x","loss_gradients_var_y"]:
+            df = dataset[categorical==val]
+            g = sns.scatterplot(data=df, x="test_acc", y=y, 
+                                size="posterior_samples", hue="test_acc", alpha=0.85, 
+                                sizes=(20, 100), #style="posterior_samples", 
+                                ax=ax[idx], legend=False, palette=cmap)
+            g.set(xlim=(60, None))
+            # ax[idx].set_title(str(val)+" samples")
+            ax[idx].set_xlabel("")
+            ax[idx].set_ylabel("")
+    # fig.text(0.5, 0.01, '(%) ', ha='center', fontsize=12)
+    # fig.text(0.03, 0.5, 'Expected loss gradients components', va='center',fontsize=12,
+    #                                                            rotation='vertical')
+    # ax.legend(loc='upper right', title="posterior samples")
+
+    # fig.text(0.5, 0.01, "Samples involved in the expectations ($w \sim p(w|D)$)", ha='center')
+
+    filename = "halfMoons_lossGrads_compVariance_"+str(test_points)+".png"
+    os.makedirs(os.path.dirname(TESTS), exist_ok=True)
+    plt.savefig(TESTS + filename)
+
 
 
 def main(args):
@@ -144,7 +292,7 @@ def main(args):
 
     # === grid search + plot ===
 
-    hidden_size = [32]#128, 512] #32
+    hidden_size = [32, 128]# [512] 
     activation = ["leaky"]
     architecture = ["fc2"]
     inference = ["svi"]
@@ -154,17 +302,21 @@ def main(args):
     warmup = [None]
     n_inputs = [1000, 5000, 10000]
     posterior_samples = [1, 10, 50]
-
     init = (hidden_size, activation, architecture, inference, 
             epochs, lr, n_samples, warmup, n_inputs, posterior_samples)
 
     # parallel_grid_search(*init)
 
-    test_points = 173
-    
-    dataset = build_dataset(*init, device="cuda", test_points=test_points)
+    test_points = 113 
+    # dataset = build_components_dataset(*init, device="cuda", test_points=test_points, rel_path=DATA)
     dataset = pandas.read_csv(TESTS+"halfMoons_lossGrads_gridSearch_"+str(test_points)+".csv")
-    scatterplot_gridSearch_gradComponents(dataset=dataset, device="cuda", test_points=test_points)
+    # scatterplot_gridSearch_gradComponents(dataset=dataset, device="cuda", test_points=test_points)
+    scatterplot_gridSearch_samp_vs_hidden(dataset=dataset, device="cuda", test_points=test_points)
+    
+    # test_points = 500
+    # # dataset = build_variance_dataset(*init, device="cuda", test_points=test_points, rel_path=DATA)
+    # dataset = pandas.read_csv(TESTS+"halfMoons_lossGrads_compVariance_"+str(test_points)+".csv")
+    # scatterplot_gridSearch_variance(dataset, test_points, device="cuda")
 
 
 if __name__ == "__main__":
